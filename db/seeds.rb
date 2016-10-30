5c988f73ab4dffe5fbb8657d1b9222ce0697a30b
zwolin <p.zwolin@gmail.com>
2016-10-30 20:11:09 +0100

    Add seeds (data for the database)
    Add 25 students, 5 subject items, 3 teachers.

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,7 +2,7 @@ puts "Seeds: start"
 TEACHER_TITLES = %w(Dr. Prof. TA)
 User.create!(email: 'admin@admin.com',password: 'adminadmin')
 
-30.times do
+3.times do
   Teacher.create!(
     first_name: Faker::Name.first_name,
     last_name: Faker::Name.last_name,
@@ -11,14 +11,14 @@ User.create!(email: 'admin@admin.com',password: 'adminadmin')
 end
 
 teachers = Teacher.all
-20.times do
+['Computer Science','Maths','English','Law','Chemistry'].each do |subject_item|
   SubjectItem.create!(
-    title: Faker::Lorem.sentence,
+    title: subject_item,
     teacher: teachers.sample
   )
 end
 
-40.times do
+25.times do
   Student.create!(
     first_name: Faker::Name.first_name,
     last_name: Faker::Name.last_name
