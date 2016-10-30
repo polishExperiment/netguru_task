3773f6dab23a4dfb39bd39337f629355d844ff6f
zwolin <p.zwolin@gmail.com>
2016-10-30 15:27:46 +0100

    Add devise to user so that one can register and sign-in
    Now devise is configured so that it enables authentication
     via User model.
    
    Fixed missing student_id in db schema, so now it was possible to
    load whole schema without an error.
    
    Also added some routes that made it impossible to view app in
    a browser, like root, teachers and report.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
+  devise_for :users
+
+  root to: 'visitors#index'
+
   resources :students do
     get :subjects
   end
+
+  resources :teachers
+
+  get 'report/subjects', to: 'reports#subjects'
 end
